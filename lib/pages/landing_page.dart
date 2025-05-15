f28599c2d32b81454e29d2cd518fe0708b6d8346
nicoplanas <planas.nicolas@correo.unimet.edu.ve>
2025-05-15 02:47:06 -0400

    Configuración de Firebase y creación de Landing Page 🦧

---
+++ lib/pages/landing_page.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import '../widgets/navbar.dart';
+import '../landing/info_section.dart';
+
+class LandingPage extends StatelessWidget {
+  const LandingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        final user = snapshot.data;
+        final email = user?.email ?? 'Guest';
+
+        return Scaffold(
+          body: ListView(
+            children: [
+              Navbar(email: email),
+              InfoSection(),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
